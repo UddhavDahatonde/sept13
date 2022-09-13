d3d1d525e66fc517a6b1a84de0aa61c923877058
Uddhav Dahatonde <dahatondeuddhava@gmail.com>
2022-09-13 18:34:12 +0530

    Add project files.

---
+++ Date.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+    public class Date
+    {
+        int day, year;
+        string month;
+
+        public void AssignValue()
+        {
+            day = 13;
+            month = "sept";
+            year = 2022;
+        }
+        public string DisplayValue()
+        {
+            return $"Date is :{day}/{month}/{year}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Date date= new Date();
+            date.AssignValue();
+            string data = date.DisplayValue();
+            Console.WriteLine(data);
+        }
+    }
+}
